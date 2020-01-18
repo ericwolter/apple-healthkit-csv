7c5008eb681c818d800a9dda1ba6ad4ba408c7d2
Eric <eric.wolter@gmx.de>
2020-01-18 18:46:39 +0100

    adds google analytics tag properly

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 mkdir dist
 rm dist/*
 npx html-inline -i src/index.html -o dist/index.inline.html
-awk '/<\/head>/{print "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"}1' dist/index.inline.html > dist/index.ads.html
+awk '/<head>/{print;print "<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-42589062-1\"></script>";next}1' dist/index.inline.html > dist/index.analytics.html
+awk '/<\/head>/{print "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"}1' dist/index.analytics.html > dist/index.ads.html
 npx html-minifier -o dist/index.min.html -c htmlmin.json dist/index.ads.html
