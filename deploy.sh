45598cd4d1d78d9d09cbcb41518bfe4bb55776c3
Eric <eric.wolter@gmx.de>
2020-02-28 21:42:06 +0100

    fixes external links in minify

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ -d "dist/" ]; then
   rm -Rf "dist/";
 fi
 mkdir "dist/"
-npx html-inline -i src/index.html -o dist/index.inline.html
+npx html-inline -i src/index.html -o dist/index.inline.html --ignore-external
 awk '/<head>/{print;print "<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-42589062-1\"></script>";next}1' dist/index.inline.html > dist/index.analytics.html
 awk '/<\/head>/{print "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"}1' dist/index.analytics.html > dist/index.ads.html
 mkdir "../dist/projects/apple-health-export"
